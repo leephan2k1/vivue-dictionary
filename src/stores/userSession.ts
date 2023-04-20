b12a9ee06fbfe3a965de75b7431d8f1b38dcb78d
leephan2k1 <pttl2k1@gmail.com>
2023-04-21 03:41:46 +0700

    feat: 🎸 handle logout

--- src/stores/userSession.ts
+++ src/stores/userSession.ts
@@ -4,8 +4,11 @@ import type { SessionStatus } from '@/types/app';
 import axios from 'axios';
 import getAPIUrl from '@/utils/getAPIUrl';
 import type { User } from '@/types/app';
+import { useRouter } from 'vue-router';
+import { toast } from 'vue-sonner';
 
 export const useSession = defineStore('userSession', () => {
+  const router = useRouter();
   const status = ref<SessionStatus>('loading');
   const user = ref<User | null>(null);
 
@@ -21,5 +24,23 @@ export const useSession = defineStore('userSession', () => {
     }
   });
 
-  return { status, user };
+  const logout = async () => {
+    const fallback = user.value;
+    try {
+      status.value = 'unauthenticated';
+      user.value = null;
+
+      await axios.get(`${getAPIUrl()}/api/logout`, { withCredentials: true });
+
+      router.push({ name: 'home' });
+    } catch (error) {
+      toast.error('Oops!, có gì đó không đúng? Thử lại nhé :>');
+      console.error(error);
+      //reverse value
+      status.value = 'authenticated';
+      user.value = fallback;
+    }
+  };
+
+  return { status, user, logout };
 });

dccbde35db6150d5fb87aca5a3f3d620cad05137
leephan2k1 <pttl2k1@gmail.com>
2023-04-21 03:42:44 +0700

    feat: 🎸 add user menu & handle logout event

---
+++ src/components/shared/UserMenuModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <Teleport to="body">
+    <TransitionRoot
+      :show="props.open"
+      as="template"
+      enter="duration-300 ease-out"
+      enter-from="opacity-0"
+      enter-to="opacity-100"
+      leave="duration-200 ease-in"
+      leave-from="opacity-100"
+      leave-to="opacity-0"
+    >
+      <div class="fixed inset-0 bg-black bg-opacity-25 z-30" />
+    </TransitionRoot>
+  </Teleport>
+
+  <TransitionRoot
+    ref="target"
+    appear
+    :show="props.open"
+    as="template"
+    enter="duration-300 ease-out"
+    enter-from="opacity-0 scale-95"
+    enter-to="opacity-100 scale-100"
+    leave="duration-200 ease-in"
+    leave-from="opacity-100 scale-100"
+    leave-to="opacity-0 scale-95"
+  >
+    <div
+      class="absolute top-[130%] space-y-4 py-6 px-3 right-0 rounded-xl h-fit w-[13rem] bg-black z-[500] flex flex-col z-40"
+    >
+      <button class="p-3 hover:bg-main-background rounded-xl flex items-center space-x-2">
+        <InformationCircleIcon class="w-7 h-7" />
+        <span>Thông tin</span>
+      </button>
+      <button
+        @click.stop="handleLogout"
+        class="p-3 hover:bg-main-background rounded-xl flex items-center space-x-2"
+      >
+        <SolarLogout2Bold class="rotate-180" />
+        <span>Đăng xuất</span>
+      </button>
+    </div>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { TransitionRoot } from '@headlessui/vue';
+import { onClickOutside } from '@vueuse/core';
+import SolarLogout2Bold from '@/components/icons/SolarLogout2Bold.vue';
+import { InformationCircleIcon } from '@heroicons/vue/20/solid';
+import { useSession } from '@/stores/userSession';
+
+const props = defineProps<{ open: boolean }>();
+const emits = defineEmits(['setOpen']);
+
+const target = ref(null);
+const sessionStore = useSession();
+
+const handleLogout = () => {
+  emits('setOpen', false);
+  sessionStore.logout();
+};
+
+onClickOutside(target, () => {
+  emits('setOpen', false);
+});
+</script>

372339255ecce2ab6a10ca7323ff7abe54eb3689
leephan2k1 <pttl2k1@gmail.com>
2023-04-24 18:56:29 +0700

    feat: 🎸 mutate history trans when user logged in

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,54 @@
 <script setup lang="ts">
-import { computed } from 'vue';
 import { PUBLIC_LAYOUT } from '@/constants';
+import type { TranslationHistory } from '@/types/app';
+import { useMutation } from '@tanstack/vue-query';
+import { useStorage } from '@vueuse/core';
+import axios from 'axios';
+import { computed, ref, watch } from 'vue';
 import { RouterView, useRoute } from 'vue-router';
+import { useSession } from './stores/userSession';
+import getAPIUrl from './utils/getAPIUrl';
 
 const route = useRoute();
+const session = useSession();
+const isUpdateHistory = ref(false);
+const localHistory = useStorage<TranslationHistory[]>('translations-history', []);
+
+const { mutate: addTranslationHistory } = useMutation({
+  mutationFn: ({ payload }: { payload: TranslationHistory[] }) =>
+    axios.post(
+      `${getAPIUrl()}/api/users/translation-history`,
+      {
+        translations_history: payload,
+        word: '',
+        sense: '',
+        targetLanguage: '',
+        currentLanguage: ''
+      },
+      { withCredentials: true }
+    ),
+  onSuccess: () => {
+    isUpdateHistory.value = true;
+    localHistory.value = [];
+  }
+});
 
 // support by assign global component
 const layout = computed(() => (route.meta.layout || PUBLIC_LAYOUT) + '-layout');
+
+// update history translations to remote server after login:
+watch(
+  () => session.status,
+  () => {
+    if (
+      session.status === 'authenticated' &&
+      !isUpdateHistory.value &&
+      localHistory.value.length > 0
+    ) {
+      addTranslationHistory({ payload: localHistory.value });
+    }
+  }
+);
 </script>
 
 <template>

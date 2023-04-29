86ef256ab4dee4494c852b1baebd023fe3a99e69
leephan2k1 <pttl2k1@gmail.com>
2023-04-29 13:16:13 +0700

    feat: 🎸 test load balancer

---
+++ src/utils/proxyBalancer.ts
@@ -0,0 +1,19 @@
+/*
+Because of glosbe dictionary tracking rate limit
+So, i deploy multiple node server to "bypass" glosbe
+It was inspired "Round-Robin Load Balancing"
+Skip it if you can't deploy multiple node server ;)
+*/
+import getAPIUrl from './getAPIUrl';
+
+const servers: string[] = JSON.parse(String(import.meta.env.VITE_UP_STREAM_SERVERS));
+const main_server = getAPIUrl();
+
+let current = -1;
+
+export default function roundRobinServer() {
+  if (!servers || servers.length === 0) return main_server;
+
+  current === servers.length - 1 ? (current = 0) : current++;
+  return `${servers[current]}/api`;
+}

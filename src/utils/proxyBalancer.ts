181f34db41aeb8dfd45b489be1a5d4434a3e5a85
leephan2k1 <pttl2k1@gmail.com>
2023-04-29 14:16:50 +0700

    fix: 🐛 safe parse

--- src/utils/proxyBalancer.ts
+++ src/utils/proxyBalancer.ts
@@ -6,13 +6,15 @@ Skip it if you can't deploy multiple node server ;)
 */
 import getAPIUrl from './getAPIUrl';
 
-const servers: string[] = JSON.parse(String(import.meta.env.VITE_UP_STREAM_SERVERS));
+const servers: string[] = import.meta.env.VITE_UP_STREAM_SERVERS
+  ? JSON.parse(String(import.meta.env.VITE_UP_STREAM_SERVERS))
+  : [];
 const main_server = getAPIUrl();
 
 let current = -1;
 
 export default function roundRobinServer() {
-  if (!servers || servers.length === 0) return main_server;
+  if (!servers || servers.length === 0) return `${main_server}/api`;
 
   current === servers.length - 1 ? (current = 0) : current++;
   return `${servers[current]}/api`;
